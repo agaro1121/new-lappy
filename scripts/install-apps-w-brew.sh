220a646cfd792b0b49e5b53ca85c7768f2dc7d2b
Anthony Garo <agaro1121@gmail.com>
2018-12-03 19:31:38 -0500

    working copy

---
+++ scripts/install-apps-w-brew.sh
@@ -0,0 +1,48 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && \
+brew tap caskroom/cask && \
+brew cask install iterm2 && \
+brew cask install homebrew/cask-fonts/font-firacode-nerd-font && \
+brew cask install homebrew/cask-fonts/font-firacode-nerd-font-mono && \
+brew cask install google-chrome && \
+brew cask install spectacle && \
+brew cask install sublime-text && \
+brew cask install firefox && \
+brew install git && \
+brew cask install font-monofur-nerd-font && brew cask install font-monofur-nerd-font-mono && \
+brew cask install font-hasklig-nerd-font && brew cask install font-hasklig-nerd-font-mono && \
+brew cask install jetbrains-toolbox && \
+brew cask install alfred && \
+brew cask install simplenote && \
+brew install homebrew/cask-versions/java8 && \
+brew cask install dropbox && \
+brew install sbt && \
+brew cask install slack && \
+brew cask install visual-studio-code && \
+brew cask install gitter && \
+brew cask install dash && \
+brew cask install sourcetree && \
+brew cask install boostnote && \
+brew install docker && \
+brew cask install vlc && \
+brew install watch && \
+brew install transmission && \
+brew cask install transmission && \
+brew install diff-so-fancy &&  \
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX" && \
+git config --global color.ui true && \
+git config --global color.diff-highlight.oldNormal    "red bold" && \
+git config --global color.diff-highlight.oldHighlight "red bold 52" && \
+git config --global color.diff-highlight.newNormal    "green bold" && \
+git config --global color.diff-highlight.newHighlight "green bold 22" && \
+git config --global color.diff.meta       "yellow" && \
+git config --global color.diff.frag       "magenta bold" && \
+git config --global color.diff.commit     "yellow bold" && \
+git config --global color.diff.old        "red bold" && \
+git config --global color.diff.new        "green bold" && \
+git config --global color.diff.whitespace "red reverse" && \
+brew install ammonite-repl && \
+brew install scala && \
+brew cask install private-internet-access && \
+brew cask install 1password && \
+brew cask install dashlane && \
+brew cask install cinch
